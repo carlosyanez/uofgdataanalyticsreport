f6d4197750755c5a3c280e8687452594e093db82
carlosyanez <carlos.yanez.s@gmail.com>
2021-09-21 08:29:06 +0200

    +vignette

---
+++ inst/doc/howto.R
@@ -0,0 +1,10 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+knitr::opts_chunk$set(include = FALSE)
+library(uofgdataanalyticsreport)
+
